43eb36553f5681bca5f96caa132b57ac2427b99e
sdardew <sdardew@gmail.com>
2021-09-05 20:40:13 -0700

    Connect Web3

--- src/App.js
+++ src/App.js
@@ -6,6 +6,38 @@ import Web3 from 'web3';
 
 
 class App extends Component {
+
+  async componentDidMount() {
+
+  }
+
+  initWeb3 = async () => {
+    if (window.ethereum) {
+      console.log('Recent mode')
+      this.web3 = new Web3(window.ethereum);
+      try {
+          // Request account access if needed
+          await window.ethereum.enable();
+          // Acccounts now exposed
+          // this.web3.eth.sendTransaction({/* ... */});
+      } catch (error) {
+          // User denied account access...
+          console.log(`User denied account access error : ${error}`)
+      }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      console.log('legacy mode')
+      this.web3 = new Web3(Web3.currentProvider);
+      // Acccounts always exposed
+      // web3.eth.sendTransaction({/* ... */});
+    }
+    // Non-dapp browsers...
+    else {
+      console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
+  }
+
   render() {
     return (
       <div>
