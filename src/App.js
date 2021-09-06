a629c250175907935437b7fe016ed17ec731d792
sdardew <sdardew@gmail.com>
2021-09-05 21:19:04 -0700

    Complete settings

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ class App extends Component {
 
   async componentDidMount() {
     await this.initWeb3()
+    console.log(this.web3);
+    let accounts = await this.web3.eth.getAccounts();
+    console.log(accounts);
+
+    let balance = await this.web3.eth.getBalance(accounts[0]);
+    console.log(balance);
   }
 
   initWeb3 = async () => {
