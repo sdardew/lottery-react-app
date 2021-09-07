188f200552cd5389e064868a8ed5e13a4c7ec47a
sdardew <sdardew@gmail.com>
2021-09-06 22:01:01 -0700

    Implement Markup

--- src/App.js
+++ src/App.js
@@ -9,6 +9,26 @@ let lotteryABI = [ { "inputs": [], "payable": false, "stateMutability": "nonpaya
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      betRecords: [], // betting result
+      winRecords: [],
+      failRecords: [],
+      pot: '0',
+      challenges: ['A', 'B'],
+      finalRecords: [{
+        bettor:'0xabcd...',
+        index:'0',
+        challenges:'ab',
+        answer:'ab',
+        targetBlockNumber:'10',
+        pot:'0'
+      }]
+    }
+  }
+
   async componentDidMount() {
     await this.initWeb3()
     await this.getBetEvents()
@@ -56,6 +76,14 @@ class App extends Component {
     this.lotteryContract.methods.betAndDistribute('0xcd').send({from:this.account, value:5000000000000000, gas:300000})
   }
 
+  getPot = async () => {
+    
+    let pot = await this.lotteryContract.methods.getPot().call();
+    let potString = this.web3.utils.fromWei(pot.toString(), 'ether');
+    this.setState({pot:potString})
+
+  }
+
   getBetEvents = async () => {
     const records = [];
     let events = await this.lotteryContract.getPastEvents('BET', {fromBlock:0, toBlock:'latest'});
@@ -68,13 +96,103 @@ class App extends Component {
     this.lotteryContract.methods.betAndDistribute('0xcd').send({from: this.account, value: 5000000000000000, gas:300000, nonce:nonce})
   }
 
+  // Pot money
+
+  // bet 글자 선택 UI(버튼 형식)
+  // Bet button
+
+  // History table
+  // index address challenge answer pot status answerBlockNumber
+
+  getCard = (_Character, _cardStyle) => {
+    let _card = '';
+
+    if(_Character === 'A'){
+      _card = '🂡'
+    }
+    if(_Character === 'B'){
+      _card = '🂱'
+    }
+    if(_Character === 'C'){
+      _card = '🃁'
+    }
+    if(_Character === 'D'){
+      _card = '🃑'
+    }
+
+    return (
+      <button className={_cardStyle} onClick = {() => {this.onClickCard(_Character)}}>
+        <div className ="card-body text-center">
+          <p className="card-text"></p>
+          <p className="card-text text-center" style={{fontSize:200}}>{_card}</p>
+          <p className="card-text"></p>
+        </div>
+      </button>
+    )
+  }
+
   render() {
     return (
       <div className="App">
+        {/* Header - Pot, Betting characters */}
         <div className="container">
           <div className="jumbotron">
-            lottery
+            <h1>Current Pot : {this.state.pot}</h1>
+            <p>Lottery</p>
+            <p>Lottery tutorial</p>
+            <p>Your Bet</p>
+            <p>{this.state.challenges[0]} {this.state.challenges[1]}</p>
           </div>
+        </div>
+        {/* Card section */}
+        <div className="container">
+          <div className="card-group">
+            {this.getCard('A', 'card bg-primary')}
+            {this.getCard('B', 'card bg-warning')}
+            {this.getCard('C', 'card bg-danger')}
+            {this.getCard('D', 'card bg-success')}
+          </div>
+        </div>
+
+        <br></br>
+
+        <div className="container">
+          <button className="btn btn-danger btn-lg" onClick={this.bet}>BET!</button>
+        </div>
+
+        <br></br>
+
+        <div className="container">
+          <table className="table table-dark table-striped">
+            <thead>
+              <tr>
+                <th>Index</th>
+                <th>Address</th>
+                <th>Challenge</th>
+                <th>Answer</th>
+                <th>Pot</th>
+                <th>Status</th>
+                <th>AnswerBlockNumber</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                this.state.finalRecords.map((record, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>{record.index}</td>
+                      <td>{record.bettor}</td>
+                      <td>{record.challenges}</td>
+                      <td>{record.answer}</td>
+                      <td>{record.pot}</td>
+                      <td>{record.win}</td>
+                      <td>{record.targetBlockNumber}</td>
+                    </tr>
+                  )
+                })
+              }
+            </tbody>
+          </table>
         </div>
       </div>
     )
